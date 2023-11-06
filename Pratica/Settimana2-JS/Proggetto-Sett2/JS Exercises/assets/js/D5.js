7822cc94567b25cd357cc75f47f7365f1aeb6ba3
FlorLizardo <florencializardo7@gmail.com>
2023-11-06 16:39:31 +0100

    Proggetto Settimana2: Modifiche Finali

--- Pratica/Settimana2-JS/Proggetto-Sett2/JS Exercises/assets/js/D5.js
+++ Pratica/Settimana2-JS/Proggetto-Sett2/JS Exercises/assets/js/D5.js
@@ -10,73 +10,72 @@ REGOLE
 /* ESERCIZIO 1
     Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
 */
-const pets = ['dog', 'cat', 'hamster', 'redfish']
+const pets = ["dog", "cat", "hamster", "redfish"];
 
-console.log('Esercizio 1: ');
+console.log("Esercizio 1: ");
 console.log(pets);
 
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
 
-  let arrayOrdinato = pets.sort();
+let arrayOrdinato = pets.sort();
 
-  console.log('Esercizio 2: ');
-  console.log(arrayOrdinato);
+console.log("Esercizio 2: ");
+console.log(arrayOrdinato);
 
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
 
-  pets.reverse();
-  console.log('Esercizio 3: ')
-  console.log(pets);
+pets.reverse();
+console.log("Esercizio 3: ");
+console.log(pets);
 
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
 
-  /*let finale = pets.shift();
+/*let finale = pets.shift();
   pets.push(finale);
   console.log('Esercizio 4: ');
   console.log(pets);*/
 
+let elementFinale = pets.slice(1);
+elementFinale.push(pets[0]);
 
-  let elementFinale = pets.slice(1);
-  elementFinale.push(pets[0]);
-
-  console.log('Esercizio 4: ');
-  console.log(elementFinale);
+console.log("Esercizio 4: ");
+console.log(elementFinale);
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
 */
 const cars = [
-  {
-    brand: 'Ford',
-    model: 'Fiesta',
-    color: 'red',
-    trims: ['titanium', 'st', 'active'],
-  },
-  {
-    brand: 'Peugeot',
-    model: '208',
-    color: 'blue',
-    trims: ['allure', 'GT'],
-  },
-  {
-    brand: 'Volkswagen',
-    model: 'Polo',
-    color: 'black',
-    trims: ['life', 'style', 'r-line'],
-  },
-]
+	{
+		brand: "Ford",
+		model: "Fiesta",
+		color: "red",
+		trims: ["titanium", "st", "active"],
+	},
+	{
+		brand: "Peugeot",
+		model: "208",
+		color: "blue",
+		trims: ["allure", "GT"],
+	},
+	{
+		brand: "Volkswagen",
+		model: "Polo",
+		color: "black",
+		trims: ["life", "style", "r-line"],
+	},
+];
 
-cars[0].licensePlate = 'AB456';
-cars[1].licensePlate = 'CD456';
-cars[2].licensePlate = 'FG456';
+cars[0].licensePlate = "AB456";
+cars[1].licensePlate = "CD456";
+cars[2].licensePlate = "FG456";
 
-console.log('Esercizio 5: ');
+console.log("Esercizio 5: ");
 console.log(cars);
 
 /* ESERCIZIO 6
@@ -85,35 +84,31 @@ console.log(cars);
 */
 
 let newOggetto = {
-    brand: 'Fiat',
-    model: 'Punto',
-    color: 'white',
-    trims: ['life', 'style'],
-    licensePlate: 'HI456'
-}
- cars.push(newOggetto);
+	brand: "Fiat",
+	model: "Punto",
+	color: "white",
+	trims: ["life", "style"],
+	licensePlate: "HI456",
+};
+cars.push(newOggetto);
 
- 
 for (let i = 0; i < cars.length; i++) {
-  cars[i].trims.pop();
+	cars[i].trims.pop();
 }
 
-
-console.log('Esercizio 6: ');
+console.log("Esercizio 6: ");
 console.log(cars);
-
 
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
-const justTrims = []
+const justTrims = [];
 
-for(let i = 0; i < cars.length; i++) {
-  justTrims.push(cars[i].trims[0]);
-
+for (let i = 0; i < cars.length; i++) {
+	justTrims.push(cars[i].trims[0]);
 }
 
-console.log('Esercizio 7: ');
+console.log("Esercizio 7: ");
 console.log(justTrims);
 
 /* ESERCIZIO 8
@@ -121,31 +116,29 @@ console.log(justTrims);
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
 
-console.log('Esercizio 8: ')
+console.log("Esercizio 8: ");
 for (let i = 0; i < cars.length; i++) {
-  let primaLettera = cars[i].color.charAt(0);
-  if(primaLettera === 'b') {
-    console.log('Fizz');
-  }else{
-    console.log('Buzz');
-  }
+	let primaLettera = cars[i].color.charAt(0);
+	if (primaLettera === "b") {
+		console.log("Fizz");
+	} else {
+		console.log("Buzz");
+	}
 }
-
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
 const numericArray = [
-  6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
-]
+	6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
+];
 
-console.log('Esercizio 9: ');
+console.log("Esercizio 9: ");
 let valori = 0;
 
-while(valori < numericArray.length && numericArray[valori] !== 32){
-  
-  console.log(numericArray[valori]);
-  valori++;
+while (valori < numericArray.length && numericArray[valori] !== 32) {
+	console.log(numericArray[valori]);
+	valori++;
 }
 
 /* ESERCIZIO 10
@@ -153,35 +146,82 @@ while(valori < numericArray.length && numericArray[valori] !== 32){
     dell'alfabeto italiano.
     es. [f, b, e] --> [6, 2, 5]
 */
-const charactersArray = ['g', 'n', 'u', 'z', 'd']
+const charactersArray = ["g", "n", "u", "z", "d"];
 
-function lettere(){
-  let arrayLettere = [];
-  
-  for(let i = 0; i < charactersArray.length; i++) {
-  
-    switch(charactersArray[i]) {
-      case 'g':
-        arrayLettere.push(7);
-        break;
-      case 'n':
-        arrayLettere.push(12);
-        break;
-      case 'u':
-        arrayLettere.push(19);
-        break;
-      case 'z':
-        arrayLettere.push(21);
-        break
-      case 'd':
-        arrayLettere.push(4);
-        break;
-      default:
-        arrayLettere.push('Non trovato nell\'array');
-    }
-  }
-  return arrayLettere;
+function lettere() {
+	let arrayLettere = [];
+
+	for (let i = 0; i < charactersArray.length; i++) {
+		switch (charactersArray[i]) {
+			case "a":
+				arrayLettere.push(1);
+				break;
+			case "b":
+				arrayLettere.push(2);
+				break;
+			case "c":
+				arrayLettere.push(3);
+				break;
+			case "d":
+				arrayLettere.push(4);
+				break;
+			case "e":
+				arrayLettere.push(5);
+				break;
+			case "f":
+				arrayLettere.push(6);
+				break;
+			case "g":
+				arrayLettere.push(7);
+				break;
+			case "h":
+				arrayLettere.push(8);
+				break;
+			case "i":
+				arrayLettere.push(9);
+				break;
+			case "l":
+				arrayLettere.push(10);
+				break;
+			case "m":
+				arrayLettere.push(11);
+				break;
+			case "n":
+				arrayLettere.push(12);
+				break;
+			case "o":
+				arrayLettere.push(13);
+				break;
+			case "p":
+				arrayLettere.push(14);
+				break;
+			case "q":
+				arrayLettere.push(15);
+				break;
+			case "r":
+				arrayLettere.push(16);
+				break;
+			case "s":
+				arrayLettere.push(17);
+				break;
+			case "t":
+				arrayLettere.push(18);
+				break;
+			case "u":
+				arrayLettere.push(19);
+				break;
+			case "v":
+				arrayLettere.push(20);
+				break;
+			case "z":
+				arrayLettere.push(21);
+				break;
+			default:
+				arrayLettere.push("Non trovato nell'array");
+		}
+	}
+	return arrayLettere;
 }
 
-console.log('Esercizio 10: ');
+console.log("Esercizio 10: ");
 console.log(lettere());
