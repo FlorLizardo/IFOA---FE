f1a143be7e18772a3de30f08537771e5251f78c0
FlorLizardo <florencializardo7@gmail.com>
2023-11-27 21:48:04 +0100

    Settimana 5, Progetto: lettere M - Ora: 21:50

--- Pratica/Settimana5-html-css/Progetto-Settimanale/assets/js/script.js
+++ Pratica/Settimana5-html-css/Progetto-Settimanale/assets/js/script.js
@@ -24,4 +24,22 @@ window.addEventListener('scroll', function(){
   previY = window.scrollY;
 });
 
+/*Lettere M*/
 
+document.addEventListener("DOMContentLoaded", function () {
+ 
+  const lettere = document.querySelectorAll("#lettere g[opacity='0']");
+  
+  function lettereM() {
+    lettere.forEach(element => {
+     
+      setTimeout(() => {
+        element.style.opacity = (element.style.opacity === "0") ? "1" : "0";
+      }, Math.random() * 10000); 
+    });
+
+    setTimeout(lettereM, 5000);
+  }
+
+  lettereM();
+});
